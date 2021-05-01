3d16c2b5db1e0734b9dfacba6526c0a51772ba03
Yulia <ukalinicenko15@gmail.com>
2021-05-01 20:53:41 +0300

    Added new project

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p> Float in the Dead Sea, see a broadway musical, stand in front of the Taj Mahal or trek the Inca Trail to Machu
+      Picchu-what’s on your ultimate life list? It’s never too early to start a list of incredible things to do before
+      you die, and here are 100 of my favorite.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
---
+++ src/views/Todos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <hr>
+    <AddTodo
+        @add-todo="addTodo"
+    />
+    <hr>
+    <TodoList
+        v-bind:todos="todos"
+        @remove-todo="removeTodo" />
+  </div>
+</template>
+
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Take a Christmas Market Cruise Through Europe', completed: false},
+        {id: 2, title: 'Make a Wish in the Trevi Fountain in Italy', completed: false},
+        {id: 3, title: 'See the Changing of the Guard in London', completed: false}
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo
+  }
+}
+</script>
